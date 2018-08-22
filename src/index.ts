97f011d8d7ee8570d33c8f021b1adf9e8ecca3f2
Stephen Haberman <stephen.haberman@gmail.com>
2018-08-22 10:59:08 -0500

    Reorganize for readability.

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const typeDefs = gql`
   }
 `;
 
-// Regular code generated GQL types
+// These are the regular code-generated GQL types, written by hand for now
 type Employee = {
   uuid: string;
   firstName: string | null;
@@ -32,7 +32,7 @@ type Employer = {
   name: string;
 };
 
-// The code generated resolver contract
+// These are the prototype code-generated resolver contracts
 type QueryResolver = {
   hello: () => string | Promise<string>;
   helloAsync: () => string | Promise<string>;
@@ -40,11 +40,30 @@ type QueryResolver = {
   employers: () => EmployerRoot[] | Promise<EmployerRoot[]>;
 };
 
-// The hand-written implementation
+type EmployeeResolver = {
+  uuid: (root: EmployeeRoot) => string;
+  firstName: (root: EmployeeRoot) => string | null;
+};
+
+type EmployerResolver = {
+  uuid: (root: EmployerRoot) => string | Promise<string>;
+  name: (root: EmployerRoot) => string | Promise<string>;
+};
+
+// We would hand-write the "root" type alias for each resolver, e.g. what
+// the upstream resolvers must pass down to the resolver
+type EmployeeRoot = string;
+
+// This is an example of a root that could be different things, using the
+// JS/TS idiom of tagged/discriminated unions (basically ADTs if you squint)
+type EmployerRoot =
+  | { tag: "uuid"; value: string }
+  | { tag: "instance"; value: Employer };
+
+// Now we have our hand-written resolver implementations that must meet
+// the codegen'd contracts
 const QueryResolverImpl: QueryResolver = {
-  hello: () => {
-    return "Hello world!!";
-  },
+  hello: () => "Hello world!!",
 
   helloAsync: () => {
     return Promise.resolve("Hello async!!");
@@ -60,47 +79,23 @@ const QueryResolverImpl: QueryResolver = {
   }
 };
 
-// This is an example of a root that is always the primary key
-type EmployeeRoot = string;
-
-// The code generated resolver contract
-type EmployeeResolver = {
-  uuid: (root: EmployeeRoot) => string;
-
-  firstName: (root: EmployeeRoot) => string | null;
-};
-
-// The hand-written implementation
 const EmployeeResolverImpl: EmployeeResolver = {
   uuid: root => fetchEmployee(root).uuid,
-
   firstName: root => fetchEmployee(root).firstName
 };
 
-// I want this to be a private helper method inside the resolver impl, but its just a dictionary
+// Ideally this would be a private helper method inside of EmployeeResolverImpl
+// but right now everything are hashes instead of objects b/c that's what graphql wants
 function fetchEmployee(uuid: string): Employee {
   return { uuid, firstName: `employee${uuid}` };
 }
 
-// This is an example of a root that could be different things
-type EmployerRoot =
-  | { tag: "uuid"; value: string }
-  | { tag: "instance"; value: Employer };
-
-// The code generated resolver contract
-type EmployerResolver = {
-  uuid: (root: EmployerRoot) => string | Promise<string>;
-
-  name: (root: EmployerRoot) => string | Promise<string>;
-};
-
-// The hand-written implementation
 const EmployerResolverImpl: EmployerResolver = {
   uuid: root => fetchEmployerIfNeeded(root).then(root => root.uuid),
-
   name: root => fetchEmployerIfNeeded(root).then(root => root.name)
 };
 
+// This can be intelligent about "did you already fetch the object for me"?
 async function fetchEmployerIfNeeded(root: EmployerRoot): Promise<Employer> {
   switch (root.tag) {
     case "uuid":
@@ -110,12 +105,12 @@ async function fetchEmployerIfNeeded(root: EmployerRoot): Promise<Employer> {
   }
 }
 
+// Now we have the usual GraphQL resolver definition + server config/boot
 const resolvers: IResolvers = {
   Query: QueryResolverImpl,
   Employee: EmployeeResolverImpl,
   Employer: EmployerResolverImpl
 };
-
 const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 server.applyMiddleware({ app });
